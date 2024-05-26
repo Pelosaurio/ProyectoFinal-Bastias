5d5bf71a08204dff2007ae2a8094b0f34fe6897a
Alfredo Bastías <alfredobastias.91@gmail.com>
2024-05-26 17:08:51 -0400

    ServiciosCategoriaAdmin personalizado

--- project/servicios/admin.py
+++ project/servicios/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from . import models
 
-admin.site.register(models.ServiciosCategoria)
+admin.site.site_title = "Servicios"
+
+class ServiciosCategoriaAdmin(admin.ModelAdmin):
+    list_display = ("nombre", "descripcion")
+    list_display_links = ("nombre",)
+
+admin.site.register(models.ServiciosCategoria, ServiciosCategoriaAdmin)
