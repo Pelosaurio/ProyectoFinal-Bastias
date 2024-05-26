bca9dcfd6a4674eec6409ca351873fda5fde93bd
Alfredo Bastías <alfredobastias.91@gmail.com>
2024-05-25 20:26:01 -0400

    add: imagen servicios

--- project/servicios/models.py
+++ project/servicios/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class ServiciosCategoria(models.Model):
     nombre = models.CharField(max_length=200, unique=True)
     descripcion = models.CharField(max_length=300, verbose_name="descripción")
-    
+        
     def __str__(self) -> str:
         return self.nombre
     
