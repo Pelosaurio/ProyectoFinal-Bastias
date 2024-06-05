876b4712a4c1a40db37432202f52f3f9ddeb0535
Pelosaurio <165973597+Pelosaurio@users.noreply.github.com>
2024-06-05 16:53:55 -0400

    Revert "fix core.urls"
    This reverts commit ad8e7c94547cc54186e4a74ea20eb88aa5c619a9.

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,7 +1,6 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
 from django.urls import path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
@@ -14,5 +13,5 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += staticfiles_urlpatterns()
