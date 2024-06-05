ad8e7c94547cc54186e4a74ea20eb88aa5c619a9
Alfredo Bastías <alfredobastias.91@gmail.com>
2024-06-05 16:25:11 -0400

    fix core.urls

--- project/core/urls.py
+++ project/core/urls.py
@@ -1,6 +1,7 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth.views import LogoutView
 from django.urls import path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
@@ -13,5 +14,5 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(template_name="core/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
